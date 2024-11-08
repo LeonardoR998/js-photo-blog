60b1f972eb41339e89a574ff5d3f9c45a136cde9
LeonardoR998 <leonardoricci839@gmail.com>
2024-11-08 12:53:47 +0100

    fix problems

--- js/main.js
+++ js/main.js
@@ -2,17 +2,15 @@ const cardRow = document.getElementById("card-row");
 
 fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
   .then((res) => res.json())
-  .then((cards) => {
-    console.log(cards);
-
+  .then((data) => {
     let cardHTML = "";
 
-    cards.forEach((card) => {
+    data.forEach((card) => {
       cardHTML += `
         <div class="col-12 col-md-6 col-lg-4 mb-5">
-          <div class="card-wrapper position-relative h-100">
+          <div class="card-wrapper position-relative h-100" data-id="${card.id}">
             <img src="./img/pin.svg" class="pin-icon" alt="Pin Icon">
-            <div class="card h-100">
+            <div class="card h-100" data-id="${card.id}">
               <img src="${card.url}" class="card-img-top" alt="${card.title}">
               <div class="card-body">
                 <h5 class="card-title text-center">${card.title}</h5>
@@ -25,8 +23,29 @@ fetch("https://jsonplaceholder.typicode.com/photos?_limit=6")
     });
 
     cardRow.innerHTML = cardHTML;
-  })
 
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card) => {
+      card.addEventListener("click", (e) => {
+        if (e.target.closest(".close-btn")) {
+          return;
+        }
+        card.classList.add("open");
+        const closeButton = card.querySelector(".close-btn");
+        closeButton.classList.remove("d-none");
+        console.log(
+          `Card cliccata: ${card.querySelector(".card-title").innerText}`
+        );
+      });
+      const closeButton = card.querySelector(".close-btn");
+      closeButton.addEventListener("click", (e) => {
+        card.classList.remove("open");
+
+        closeButton.classList.add("d-none");
+      });
+    });
+  })
   .catch((error) => {
     console.error("Errore nel caricamento delle foto:", error);
   });
